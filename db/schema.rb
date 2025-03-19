9f394097ed91be838deadcf163b7853fdd06cff4
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-19 16:10:48 +0100

    add Comment migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_19_143955) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_19_150952) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
