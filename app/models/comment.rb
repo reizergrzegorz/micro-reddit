48f93a5b1bd2e3bff7020e75ef8eee549f081c40
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-19 17:18:00 +0100

    add asscociations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+
+    belongs_to :user
+    belongs_to :post
+
+    validates :body, presence: true, length: { minimum: 10 }
+    validates :user_id, presence: true
+    validates :post_id, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
     belongs_to :user
+    has_many :comments
 
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
+    validates :user_id, presence: true
 end
